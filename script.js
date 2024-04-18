40b1086a8fcdf43d60c85253bcee8653f7f61617
codewithtarde9370 <teja36491@gmail.com>
2024-04-18 20:57:58 +0530

    added change in css

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ listContainer.addEventListener("click", function(e){
 
 
 function saveData(){
-    localStorage.setItem(   "data",listContainer.innerHTML);
+    localStorage.setItem("data",listContainer.innerHTML);
 }
  
 function showTask(){
